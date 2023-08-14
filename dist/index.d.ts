03e919c085cca44f1f2cc8bde0b13572374da3b5
dev2820 <second28200@gmail.com>
2023-08-14 13:02:11 +0900

    chore/ build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,12 @@
 export * from "./rail";
+declare const _default: {
+    isFailed: (value: any) => value is {
+        failed: Symbol;
+        err: any;
+    };
+    rail: <Fns extends import("./type").Fn[]>(...funcs: Fns) => (args: any) => Promise<{
+        failed: Symbol;
+        err: any;
+    } | ReturnType<Fns[number]>>;
+};
+export default _default;
--- dist/rail.d.ts
+++ dist/rail.d.ts
@@ -5,4 +5,5 @@ type Failed = {
 };
 export declare const isFailed: (value: any) => value is Failed;
 export declare const rail: <Fns extends Fn[]>(...funcs: Fns) => (args: any) => Promise<Failed | ReturnType<Fns[number]>>;
+export declare const railRight: <Fns extends Fn[]>(...funcs: Fns) => (args: any) => Promise<Failed | ReturnType<Fns[0]>>;
 export {};
